1b9a48576d5d1b3bc5e76e9b4fb34633f58867c7
Derrick Holleman <derrickholleman@gmail.com>
2023-01-23 01:34:16 -0600

    add store

--- src/components/BingoTable.svelte
+++ src/components/BingoTable.svelte
@@ -1,103 +1,20 @@
 <script lang="ts">
+  import TableRow from "./TableRow.svelte";
+  import { allData } from "../stores/store";
+
   export let row1Data: string[];
   export let row2Data: string[];
   export let row3Data: string[];
   export let row4Data: string[];
 
-  let allData = [
-    ...row1Data,
-    ...row2Data,
-    ...row3Data,
-    ...row4Data,
-  ] as string[];
-
-  let selectedItems = [] as string[];
-
-  const handleToggleRowCell = (data: string) => {
-    const foundItem = allData.find((item) => item === data);
-    if (foundItem) {
-      selectedItems.push(foundItem);
-      selectedItems = selectedItems;
-    }
-  };
+  $allData = [...row1Data, ...row2Data, ...row3Data, ...row4Data] as string[];
 </script>
 
 <table>
   <tbody>
-    <tr>
-      {#each row1Data as data}
-        <td
-          on:keydown={(e) => {
-            if (e.key === "Enter") {
-              handleToggleRowCell(data);
-            }
-          }}
-          on:click={() => handleToggleRowCell(data)}
-          class:selected={selectedItems.includes(data)}>{data}</td
-        >
-      {/each}
-    </tr>
-
-    <tr>
-      {#each row2Data as data}
-        <td
-          on:keydown={(e) => {
-            if (e.key === "Enter") {
-              handleToggleRowCell(data);
-            }
-          }}
-          on:click={() => handleToggleRowCell(data)}
-          class:selected={selectedItems.includes(data)}>{data}</td
-        >
-      {/each}
-    </tr>
-
-    <tr>
-      {#each row3Data as data}
-        <td
-          on:keydown={(e) => {
-            if (e.key === "Enter") {
-              handleToggleRowCell(data);
-            }
-          }}
-          on:click={() => handleToggleRowCell(data)}
-          class:selected={selectedItems.includes(data)}>{data}</td
-        >
-      {/each}
-    </tr>
-
-    <tr>
-      {#each row4Data as data}
-        <td
-          on:keydown={(e) => {
-            if (e.key === "Enter") {
-              handleToggleRowCell(data);
-            }
-          }}
-          on:click={() => handleToggleRowCell(data)}
-          class:selected={selectedItems.includes(data)}>{data}</td
-        >
-      {/each}
-    </tr>
+    <TableRow rowData={row1Data} />
+    <TableRow rowData={row2Data} />
+    <TableRow rowData={row3Data} />
+    <TableRow rowData={row4Data} />
   </tbody>
 </table>
-
-<style>
-  td {
-    font-weight: bold;
-    text-transform: uppercase;
-    padding: 5px;
-    width: 100px;
-    font-size: 1.5vw;
-    height: 100px;
-    text-align: center;
-    border-radius: 5px;
-    cursor: pointer;
-    border: 1px solid black;
-    font-family: Verdana, Geneva, Tahoma, sans-serif;
-  }
-
-  .selected {
-    background-color: yellow;
-  }
-</style>
---
+++ src/components/TableRow.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  export let rowData: string[];
+  import { allData } from "../stores/store";
+
+  let selectedItems = [] as string[];
+
+  const handleToggleRowCell = (data: string) => {
+    const foundItem = $allData.find((item) => item === data);
+    if (foundItem) {
+      selectedItems.push(foundItem);
+      selectedItems = selectedItems;
+    }
+  };
+</script>
+
+<tr>
+  {#each rowData as data}
+    <td
+      on:keydown={(e) => {
+        if (e.key === "Enter") {
+          handleToggleRowCell(data);
+        }
+      }}
+      on:click={() => handleToggleRowCell(data)}
+      class:selected={selectedItems.includes(data)}>{data}</td
+    >
+  {/each}
+</tr>
+
+<style>
+  td {
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 5px;
+    width: 100px;
+    font-size: 1.5vw;
+    height: 100px;
+    text-align: center;
+    border-radius: 5px;
+    cursor: pointer;
+    border: 1px solid black;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  .selected {
+    background-color: yellow;
+  }
+</style>
