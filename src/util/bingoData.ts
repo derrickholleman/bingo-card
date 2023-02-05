6d60db714b4f31e01c4ae6cf32f3ea158eaa2b83
Derrick Holleman <derrickholleman@gmail.com>
2023-02-04 20:14:59 -0600

    option changes

--- src/util/bingoData.ts
+++ src/util/bingoData.ts
@@ -7,7 +7,7 @@ export const BINGO_DATA_ROW_1 = [
 ];
 
 export const BINGO_DATA_ROW_2 = [
-  "Studyy noticeably trips over his words while casting",
+  "Player gets a cruise missile killstreak",
   "Player shouts at the other team across the stage",
   "Team gets swept 3-0 in control",
   "Casters call out a player for snaking",
@@ -16,16 +16,16 @@ export const BINGO_DATA_ROW_2 = [
 
 export const BINGO_DATA_ROW_3 = [
   "Player gets a donut in S&D",
-  "Team gets 100 point clubbed in Hardpoint",
-  "Crowd noise gives away a defuse in S&D",
+  "Player gets an ace in S&D",
+  "Team wins a control with 5 lives or less",
   "Player gets turned on",
-  "Player gets a quad kill or an ace in S&D",
+  "Player gets a triple kill",
 ];
 
 export const BINGO_DATA_ROW_4 = [
   "S&D game goes to Round 11",
   "Lower seeded team beats a higher seeded team",
   "Cameraman shows the family of a player",
-  "Broadcast cuts to an unplanned break",
+  "Any sort of tech issue or delay happens",
   "Fans start a 'LETS GO ____!' chant",
 ];
